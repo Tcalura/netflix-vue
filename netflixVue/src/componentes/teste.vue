38aab306fef8ddd9eafe017a9c105d5e706fae24
Thiago Calura <thiagocalura@outlook.com>
2018-09-24 00:41:45 -0300

    abstracao dos componentes categoria e filme

---
+++ netflixVue/src/componentes/teste.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1> {{ titulo }} </h1>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: ['titulo'] 
+  }
+</script>
+
+<style lang="scss">
+
+h1{
+  color: red;
+}
+
+</style>
