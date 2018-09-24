5fb003156504484426c8de9720caa0284abf27e0
Thiago Calura <thiagocalura@outlook.com>
2018-09-24 18:31:37 -0300

    exemplo simples de rotas, criado uma api para categorias com json-server.

--- netflixVue/src/main.js
+++ netflixVue/src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+import Home from './paginas/Home.vue'
+const Contato = { template: '<p> Página de Contato </p>'}
+const SemPagina = { template: '<p>Essa página não existe! </p>'}
+
+const rotas = {
+  '/': Home,
+  '/home': Home,
+  '/contato': Contato
+}
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data:{
+    rotaAtual: window.location.pathname
+  },
+  computed:{
+    exibeComponente(){
+      return rotas[this.rotaAtual] || SemPagina
+    }
+  },
+  render (h) { return h(this.exibeComponente) }
 })
